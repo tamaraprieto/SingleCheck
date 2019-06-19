855f9d7c51e330856c9d2f86d9ae3e2775212e9f
Tamara Prieto <tamara.prieto.fernandez@gmail.com>
2019-06-19 13:15:33 +0200

    Add Coefficient of variation, KL divergence and ginkgo script for MAD calculation

---
+++ src/GinkgoMAD.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+args            = commandArgs(TRUE)
+dataset            = args[[1]]
+size		= args[[2]]
+setwd(paste("/mnt/netapp1/posadalab/APPS/ginkgo/uploads/",dataset,sep=""))
+
+genome            = "hg19"
+bm                = paste("variable_",size,"_150_bwa",sep="")
+pseudoautosomal = ""
+
+raw = read.table('data', header=TRUE, sep="\t")
+l = dim(raw)[1] # Number of bins
+w = dim(raw)[2] # Number of samples
+normal = sweep(raw+1, 2, colMeans(raw+1), '/')
+cellIDs <- colnames(normal)
+
+# Calculate MAD for selected cells
+a = matrix(0, length(cellIDs), 1)
+rownames(a) <- colnames(normal[,cellIDs])
+for(i in 1:length(cellIDs)){
+        cell = cellIDs[i]
+        #column 1: calculate MAD on subtracted values of bins that are right next to each other
+        a[i, 1] = mad(normal[-1    , cell] - normal[1:(l-1), cell])   # same as diff()
+    }
+a
+write.table(x=a,file=paste("MAD.",dataset,".txt",sep=""),quote=FALSE, sep="\t")
+

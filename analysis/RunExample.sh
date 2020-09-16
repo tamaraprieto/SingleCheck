7cb79388a12432cc46846e0df5c79b9ab40e2321
Tamara Prieto <tamara.prieto.fernandez@gmail.com>
2020-09-16 17:18:16 +0200

    Restructurate folders a bit and modify CreateInputApp

--- analysis/RunExample.sh
+++ analysis/RunExample.sh
@@ -7,7 +7,7 @@ export PATH=/mnt/netapp2/posadalab2/uvibetpf/SCCoverageUniformity/scripts:$PATH
 while read SAMPLE
 do
 #slurm_stdout=$(sbatch -p amd-shared --qos=amd-shared -t 02:00:00 SingleCheck -d "^[1-9]" -r ${REF} ${SAMPLE}_1.fastq.gz ${SAMPLE}_2.fastq.gz)
-slurm_stdout=$(sbatch -p amd-shared --qos=amd-shared -t 00:30:00 SingleCheck -d "^[1-9]" -r ${REF} ${SAMPLE}.bam)
+slurm_stdout=$(sbatch -p thinnodes,cola-corta -t 00:30:00 SingleCheck -r ${REF} ${SAMPLE}.bam)
 echo $slurm_stdout
 JOB_id=$(echo $slurm_stdout | awk '{print $4}')
 echo $SAMPLE $JOB_id >> JobIDs.Example.out
